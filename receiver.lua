36a2df77b0ab16706293eb5c13961e44c1afbcef
Arun .A.R <arunnadar1994@gmail.com>
2017-08-31 22:20:46 +0530

    some changes

--- receiver.lua
+++ receiver.lua
@@ -1,16 +1,28 @@
+pi = math.pi
+
+
+function round(x)
+  if x%2 ~= 0.5
+    return math.floor(x+0.5)
+  end
+  return x-0.5
+end
+
+
 function init()
     robot.leds.set_single_color(1,"red")
     robot.leds.set_single_color(12,"red")
     state = "waiting"
+    imp_msg = 0
+    t = 0
 end
-imp_msg = 0
 
 function step()
     log(state)
     if state == "waiting" then
-        wait()
+        t = wait()
     elseif state == "turning" then
-        turn()
+        turn(t)
     elseif state == "lunge" then
         drivest()
     end
@@ -18,12 +30,8 @@ end
 
 
 function reset()
-
 end
-
-
 function destroy()
-
 end
 
 
@@ -31,13 +39,29 @@ function wait()
     if robot.range_and_bearing[1].data[2] == 2 then
         imp_msg = robot.range_and_bearing[1]
         state = "turning"
+        t = 4.4*(math.abs(imp_msg.horizontal_bearing)/(2*pi))
+        t = round(t)
     end
+    return t
 end
 
-function turn()
+function turn(t)
     if imp_msg.horizontal_bearing >= -0.1 and imp_msg.horizontal_bearing <= 0.1 then
         state = "lunge"
-    elseif imp_msg.horizontal_bearing > 0
+    elseif imp_msg.horizontal_bearing > 0.1 then
+        robot.wheels.set_velocity(-1,1)
+        t = t - 1
+        if t == 0 then
+            state = "lunge"
+        end
+    elseif imp_msg.horizontal_bearing < -0.1 then
+        robot.wheels.set_velocity(1,-1)
+        t = t - 1
+        if t == 0 then
+            state = "lunge"
+        end
+    end
+end
 
 function drivest()
     x = 0
