0645e731ab0b4702001e87e06c6522451524e6ad
Arun .A.R <arunnadar1994@gmail.com>
2017-08-31 23:41:36 +0530

    worked

--- receiver.lua
+++ receiver.lua
@@ -1,28 +1,16 @@
-pi = math.pi
-
-
-function round(x)
-  if x%2 ~= 0.5
-    return math.floor(x+0.5)
-  end
-  return x-0.5
-end
-
-
 function init()
     robot.leds.set_single_color(1,"red")
     robot.leds.set_single_color(12,"red")
     state = "waiting"
     imp_msg = 0
-    t = 0
 end
 
 function step()
     log(state)
     if state == "waiting" then
-        t = wait()
+        wait()
     elseif state == "turning" then
-        turn(t)
+        turn()
     elseif state == "lunge" then
         drivest()
     end
@@ -39,27 +27,21 @@ function wait()
     if robot.range_and_bearing[1].data[2] == 2 then
         imp_msg = robot.range_and_bearing[1]
         state = "turning"
-        t = 4.4*(math.abs(imp_msg.horizontal_bearing)/(2*pi))
-        t = round(t)
     end
-    return t
 end
 
-function turn(t)
-    if imp_msg.horizontal_bearing >= -0.1 and imp_msg.horizontal_bearing <= 0.1 then
+function turn()
+    x = orientation_to_bearing()
+    if imp_msg.horizontal_bearing >= -0.05 and imp_msg.horizontal_bearing <= 0.05 then
         state = "lunge"
-    elseif imp_msg.horizontal_bearing > 0.1 then
-        robot.wheels.set_velocity(-1,1)
-        t = t - 1
-        if t == 0 then
-            state = "lunge"
-        end
-    elseif imp_msg.horizontal_bearing < -0.1 then
-        robot.wheels.set_velocity(1,-1)
-        t = t - 1
-        if t == 0 then
-            state = "lunge"
-        end
+    end
+    log(imp_msg.horizontal_bearing)
+    if math.abs(x - imp_msg.horizontal_bearing) <= 0.05 then
+        state = "lunge"
+    elseif imp_msg.horizontal_bearing > 0 then
+        robot.wheels.set_velocity(-0.5,0.5)
+    elseif imp_msg.horizontal_bearing < -0 then
+        robot.wheels.set_velocity(0.5,-0.5)
     end
 end
 
@@ -70,6 +52,26 @@ function drivest()
             x = robot.proximity[i].value
         end
     end
-    log(x)
     robot.wheels.set_velocity((1 - x) * 10,(1 - x) * 10)
+end
+
+function orientation_to_bearing()
+    angle = robot.positioning.orientation.angle
+    rot = robot.positioning.orientation.axis.z
+    if rot > 0 then
+        angle = math.fmod(angle,(2*math.pi))
+        if angle > math.pi then
+            angle = 2*math.pi - angle
+        end
+    end
+    if rot < 0 then
+        angle = math.fmod(angle,(2*math.pi))
+        if angle > math.pi then
+            angle = 2*math.pi - angle
+        else
+            angle = -angle
+        end
+    end
+    log(angle)
+    return angle
 end
