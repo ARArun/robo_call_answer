31fa654d62701f3b7592add705d24efecc613cc3
Arun .A.R <arunnadar1994@gmail.com>
2017-08-31 12:51:42 +0530

    seems to work

--- receiver.lua
+++ receiver.lua
@@ -1,26 +1,16 @@
 function init()
-robot.leds.set_single_color(1,"red")
-robot.leds.set_single_color(12,"red")
+    robot.leds.set_single_color(1,"red")
+    robot.leds.set_single_color(12,"red")
+    state = "waiting"
 end
-
+imp_msg = 0
 
 function step()
-    if robot.range_and_bearing[1].horizontal_bearing >=  0 and math.abs(robot.range_and_bearing[1].horizontal_bearing) >= 0.1 then
-        if robot.range_and_bearing[1].data[2] == 2 and robot.range_and_bearing[1].horizontal_bearing >=  0 then
-            robot.wheels.set_velocity(-1,1)
-        end
-    elseif robot.range_and_bearing[1].horizontal_bearing <=  0 and math.abs(robot.range_and_bearing[1].horizontal_bearing) >= 0.1 then
-        if robot.range_and_bearing[1].data[2] == 2 and robot.range_and_bearing[1].horizontal_bearing <=  0 then
-            robot.wheels.set_velocity(1,-1)
-        end
-    end
-
-    if math.abs(robot.range_and_bearing[1].horizontal_bearing) <= 0.1 then
-        if robot.range_and_bearing[1].range >= 20 then
-            robot.wheels.set_velocity(12,12)
-        else
-            robot.wheels.set_velocity(0,0)
-        end
+    log(state)
+    if state == "waiting" then
+        wait()
+    elseif state == "acting" then
+        act()
     end
 end
 
@@ -32,4 +22,43 @@ end
 
 function destroy()
 
+end
+
+
+function wait()
+    if robot.range_and_bearing[1].data[2] == 2 then
+        imp_msg = robot.range_and_bearing[1]
+        state = "acting"
+    end
+end
+
+function act()
+    if imp_msg.horizontal_bearing >=0 and math.abs(imp_msg.horizontal_bearing - robot.positioning.orientation.angle) >= 0.1 then
+        robot.wheels.set_velocity(-1,1)
+    end
+
+    if imp_msg.horizontal_bearing <=0 and math.abs(imp_msg.horizontal_bearing - robot.positioning.orientation.angle) >= 0.1 then
+        robot.wheels.set_velocity(1,-1)
+    end
+
+    if math.abs(imp_msg.horizontal_bearing - robot.positioning.orientation.angle) < 0.1 then
+        x = dist()
+        log(x)
+        if x == 0 then
+            robot.wheels.set_velocity(10,10)
+        elseif x == 1 then
+            robot.wheels.set_velocity(0,0)
+        end
+    end
+end
+
+
+function dist()
+    x = 0
+    for i = 1,24 do
+        if robot.proximity[i].value == 1 then
+            x = 1
+        end
+    end
+    return x
 end
